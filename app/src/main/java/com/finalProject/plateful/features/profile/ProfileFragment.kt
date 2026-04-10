ba700e45db9f6f2252314f7eea744d4f8731c3c7
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-04-10 22:28:40 +0300

    adding recipes counter in profile page

--- app/src/main/java/com/finalProject/plateful/features/profile/ProfileFragment.kt
+++ app/src/main/java/com/finalProject/plateful/features/profile/ProfileFragment.kt
@@ -1,22 +1,32 @@
 package com.finalProject.plateful.features.profile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.finalProject.plateful.R
 import com.finalProject.plateful.databinding.FragmentAddRecipeBinding
 import com.finalProject.plateful.databinding.FragmentProfileBinding
+import com.finalProject.plateful.features.recipe_list.RecipesListViewModel
+import kotlin.getValue
 
 class ProfileFragment : Fragment() {
     private var binding: FragmentProfileBinding? = null
+
+    private val viewModel: RecipesListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        viewModel.data.observe(viewLifecycleOwner) { recipes ->
+            binding?.myRecipesCountTextView?.text = recipes?.size?.toString() ?: "0"
+        }
 
         return binding?.root
     }
