29853cad90384d44bf3942bb01b21533d4b35a49
ponda <bashvitzben@gmail.com>
2026-04-28 01:11:22 +0300

    feat(edit recipe): commit for progress

---
+++ app/src/main/java/com/finalProject/plateful/features/edit_recipe/EditRecipeFragment.kt
@@ -0,0 +1,122 @@
+package com.finalProject.plateful.features.edit_recipe
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.finalProject.plateful.data.repositories.recipes.RecipesRepository
+import com.finalProject.plateful.databinding.FragmentEditRecipeBinding
+import com.finalProject.plateful.models.Recipe
+import com.finalProject.plateful.utils.extentions.bitmap
+import com.squareup.picasso.Picasso
+
+class EditRecipeFragment : Fragment() {
+    private var binding: FragmentEditRecipeBinding? = null
+    private var cameraLauncher: ActivityResultLauncher<Void?>? = null
+    private var hasImageChanged = false
+
+    var id: String? = null
+    var title: String? = null
+    var ingredients: String? = null
+    var instructions: String? = null
+    var imageUrl: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            this.id = it.getString(Recipe.ID_KEY)
+            this.title = it.getString(Recipe.TITLE_KEY)
+            this.ingredients = it.getString(Recipe.INGREDIENTS_KEY)
+            this.instructions = it.getString(Recipe.INSTRUCTIONS_KEY)
+            this.imageUrl = it.getString(Recipe.IMAGE_URL_KEY)
+
+            if (this.id.isNullOrEmpty()) {
+                this.dismiss()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEditRecipeBinding.inflate(inflater, container, false)
+        setupView()
+
+        cameraLauncher =
+            registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitMap ->
+                bitMap?.let {
+                    binding?.recipeImageImageView?.setImageBitmap(it)
+                    this.hasImageChanged = true
+                } ?: Toast.makeText(context, "No image captured", Toast.LENGTH_SHORT).show()
+            }
+
+        binding?.takePictureButton?.setOnClickListener {
+            cameraLauncher?.launch(null)
+        }
+
+        return binding?.root
+    }
+
+    private fun setupView() {
+        binding?.loadingIndicator?.visibility = View.GONE
+
+        binding?.recipeTitleTextInput?.setText(title)
+        binding?.recipeIngredientsTextInput?.setText(ingredients)
+        binding?.recipeInstructionsTextInput?.setText(instructions)
+
+        if (!imageUrl.isNullOrEmpty()) {
+            binding?.recipeImageImageView?.let { imageView ->
+                Picasso.get().load(imageUrl).into(imageView)
+            }
+        }
+
+        binding?.saveRecipeButton?.setOnClickListener {
+            binding?.loadingIndicator?.visibility = View.VISIBLE
+
+            val recipeTitle = binding?.recipeTitleTextInput?.text.toString()
+            val recipeIngredients = binding?.recipeIngredientsTextInput?.text.toString()
+            val recipeInstructions = binding?.recipeInstructionsTextInput?.text.toString()
+
+            id?.let { id ->
+                var newImageUrl = ""
+                var imageBitmap: Bitmap? = null
+
+                this.imageUrl?.let { imageUrl ->
+                    if (!this.hasImageChanged) {
+                        newImageUrl = imageUrl
+                    } else {
+                        binding?.recipeImageImageView?.isDrawingCacheEnabled = true
+                        binding?.recipeImageImageView?.buildDrawingCache()
+
+                        imageBitmap = binding?.recipeImageImageView?.bitmap
+                    }
+                }
+
+                val recipe = Recipe(
+                    id = id,
+                    title = recipeTitle,
+                    ingredients = recipeIngredients,
+                    instructions = recipeInstructions,
+                    imageUrl = newImageUrl,
+                    lastUpdated = null
+                )
+
+                RecipesRepository.shared.editRecipe(recipe, imageBitmap) {
+                    dismiss()
+                }
+            }
+        }
+    }
+
+
+    private fun dismiss() {
+        view?.findNavController()?.popBackStack()
+    }
+}
--- app/src/main/java/com/finalProject/plateful/features/recipe_list/recipeRowViewHolder.kt
+++ app/src/main/java/com/finalProject/plateful/features/recipe_list/recipeRowViewHolder.kt
@@ -1,7 +1,12 @@
 package com.finalProject.plateful.features.recipe_list
 
+import android.util.Log
 import android.view.View
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.finalProject.plateful.NavGraphDirections
+import com.finalProject.plateful.R
 import com.finalProject.plateful.data.repositories.recipes.RecipesRepository
 import com.finalProject.plateful.databinding.RecipeRowLayoutBinding
 import com.finalProject.plateful.models.Recipe
@@ -38,7 +43,11 @@ class RecipeRowViewHolder(
             binding.buttonsContainer.visibility = View.GONE
         }
 
-        // TODO: Implement recipe editing
+        binding.recipeEditBtn.setOnClickListener {
+            val action = NavGraphDirections.actionGlobalEditRecipeFragment(recipe.id, recipe.title, recipe.ingredients, recipe.instructions, recipe.imageUrl)
+            it.findNavController().navigate(action)
+        }
+
         binding.recipeDeleteBtn.setOnClickListener {
             RecipesRepository.shared.deleteRecipe(recipe) {
                 RecipesRepository.shared.refreshRecipes()
