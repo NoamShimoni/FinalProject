1caa050fd43dae36cb9341a5351f0039a8f45186
ponda <bashvitzben@gmail.com>
2026-05-02 00:03:10 +0300

    fix(library + recipe list): pull request

--- app/src/main/java/com/finalProject/plateful/dao/AppLocalDB.kt
+++ app/src/main/java/com/finalProject/plateful/dao/AppLocalDB.kt
@@ -6,23 +6,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import com.finalProject.plateful.base.MyApplication
 
 object AppLocalDB {
-
-    private val MIGRATION_ADD_CREATING_USER = object : Migration(1, 2) {
-        override fun migrate(db: SupportSQLiteDatabase) {
-            db.execSQL("ALTER TABLE Recipe ADD COLUMN creatingUserId TEXT NOT NULL DEFAULT ''")
-        }
-    }
-    private val MIGRATION_ADD_CREATING_USERNAME = object : Migration(2, 3) {
-        override fun migrate(db: SupportSQLiteDatabase) {
-            db.execSQL("ALTER TABLE Recipe ADD COLUMN creatingUserName TEXT NOT NULL DEFAULT ''")
-        }
-    }
-
-    private val MIGRATION_ADD_IS_DELETED = object : Migration(3, 4) {
-        override fun migrate(db: SupportSQLiteDatabase) {
-            db.execSQL("ALTER TABLE Recipe ADD COLUMN isDeleted INTEGER NOT NULL DEFAULT 0")
-        }
-    }
     val db: AppLocalDbRepository by lazy {
         val context =
             MyApplication.appContext ?: throw IllegalStateException("Context is not initialized")
@@ -31,7 +14,6 @@ object AppLocalDB {
             context,
             AppLocalDbRepository::class.java,
             "recipes.db"
-        ).addMigrations(MIGRATION_ADD_CREATING_USER).addMigrations(MIGRATION_ADD_CREATING_USERNAME).addMigrations(MIGRATION_ADD_IS_DELETED)
-            .build()
+        ).fallbackToDestructiveMigration(true).build()
     }
 }
