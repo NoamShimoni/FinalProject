5aff7d2afdca1252b1f08ddcb6a457149c13d387
ponda <bashvitzben@gmail.com>
2026-05-02 22:55:52 +0300

    fix(): pull request

--- app/src/main/java/com/finalProject/plateful/features/recommended_recipes/RecommendedRecipesViewModel.kt
+++ app/src/main/java/com/finalProject/plateful/features/recommended_recipes/RecommendedRecipesViewModel.kt
@@ -3,10 +3,13 @@ package com.finalProject.plateful.features.recommended_recipes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.finalProject.plateful.base.recipe_list.IRecipesViewModel
 import com.finalProject.plateful.data.repositories.remote_recipes.RemoteRecipesRepository
 import com.finalProject.plateful.models.Recipe
 import com.finalProject.plateful.models.RemoteRecipe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.concurrent.Executors
 
 class RecommendedRecipesViewModel : ViewModel(), IRecipesViewModel {
@@ -17,11 +20,11 @@ class RecommendedRecipesViewModel : ViewModel(), IRecipesViewModel {
     private val executor = Executors.newSingleThreadExecutor()
 
     override fun refreshRecipes() {
-        executor.execute {
+        viewModelScope.launch(Dispatchers.IO) {
             val remoteRecipes = RemoteRecipesRepository.shared.getRecipes()
-            
+
             val mappedRecipes = remoteRecipes.meals?.map { it.toRecipe() }?.toMutableList() ?: mutableListOf()
-            
+
             _data.postValue(mappedRecipes)
             isRefreshing.postValue(false)
         }
