7a71fa0c0402038dbb2aa32befa23e78d74ca7bf
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-05-04 01:34:23 +0300

    removing unused viewModel file and import

--- app/src/main/java/com/finalProject/plateful/features/recipe_details/RecipeDetailsFragment.kt
+++ app/src/main/java/com/finalProject/plateful/features/recipe_details/RecipeDetailsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.finalProject.plateful.R
 import com.finalProject.plateful.databinding.FragmentRecipeDetailsBinding
 import com.google.android.material.imageview.ShapeableImageView
 import com.squareup.picasso.Picasso
