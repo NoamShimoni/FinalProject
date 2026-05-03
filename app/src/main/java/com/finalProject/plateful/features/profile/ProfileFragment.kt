06116cb66cdf7bf185f5b4c391a4cce99d5e8c03
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-05-03 20:46:51 +0300

    remove redundant log

--- app/src/main/java/com/finalProject/plateful/features/profile/ProfileFragment.kt
+++ app/src/main/java/com/finalProject/plateful/features/profile/ProfileFragment.kt
@@ -1,7 +1,6 @@
 package com.finalProject.plateful.features.profile
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
